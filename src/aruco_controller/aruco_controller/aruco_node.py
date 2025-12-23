b8887a97c410daa0a4c1b716be97cc63126ac254
XSTEWARD <nikodem.michalowicz@student.put.poznan.pl>
2025-12-23 20:05:22 +0100

    Finalna wersja, dzial stop, i dobrze nurkuje

--- src/aruco_controller/aruco_controller/aruco_node.py
+++ src/aruco_controller/aruco_controller/aruco_node.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, JointState
 from cv_bridge import CvBridge
 import cv2
 import numpy as np
@@ -14,64 +14,96 @@ class ArucoNode(Node):
     def __init__(self):
         super().__init__('aruco_node')
         
-        #Subskrypcja kamery
+        # 1. Subskrypcja kamery
         self.subscription = self.create_subscription(
             Image,
             '/image_raw',
             self.image_callback,
             10)
         
-        #Publikator komend do robota UR5
+        # 2. Subskrypcja pozycji (zeby STOP dzialal w miejscu)
+        self.joint_state_sub = self.create_subscription(
+            JointState,
+            '/joint_states',
+            self.joint_state_callback,
+            10)
+            
+        # 3. Publikator komend
         self.publisher_ = self.create_publisher(
             JointTrajectory, 
-            '/scaled_joint_trajectory_controller/joint_trajectory', 
+            '/joint_trajectory_controller/joint_trajectory', 
             10)
 
         self.br = CvBridge()
-        
-        #Szerokosc strefy stopu
         self.dead_zone_pixels = 60 
         
-        #Nazwy stawow robota UR5e (
         self.joint_names = [
             'shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint',
             'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint'
         ]
         
-        #Zmienna zeby nie wysylac komend non-stop, tylko jak sie zmieni decyzja
+        self.current_joints = []
         self.last_command = "NONE"
+        
+        self.get_logger().info('Startuje Aruco Node V10 (KLASYK + PAMIEC)!')
 
-        self.get_logger().info('Startuje Aruco Node V5 (STEROWANIE ROBOTEM)!')
+    def joint_state_callback(self, msg):
+        if len(self.current_joints) == 0:
+            self.current_joints = [0.0] * 6
+        try:
+            name_to_pos = {name: pos for name, pos in zip(msg.name, msg.position)}
+            self.current_joints = [name_to_pos[j] for j in self.joint_names]
+        except KeyError:
+            pass
 
-    def send_robot_command(self, position_type):
-        # Jesli komenda jest taka sama jak poprzednia, ignorujemy
-        if position_type == self.last_command:
+    def stop_robot(self):
+        # Hamulec - zatrzymuje w aktualnej pozycji
+        if not self.current_joints:
             return
 
         msg = JointTrajectory()
         msg.header = Header()
         msg.header.stamp = self.get_clock().now().to_msg()
         msg.joint_names = self.joint_names
-
-        point = JointTrajectoryPoint()
         
-        #DEFINICJA POZYCJI
+        point = JointTrajectoryPoint()
+        point.positions = self.current_joints # Zostan tu gdzie jestes
+        point.time_from_start.sec = 0
+        point.time_from_start.nanosec = 500000000 # 0.5s stop
+        
+        msg.points = [point]
+        self.publisher_.publish(msg)
+
+    def send_robot_command(self, position_type):
+        # Jesli komenda ta sama co ostatnio, to ignoruj (chyba ze to STOP - stop zawsze wysylamy)
+        if position_type == self.last_command and position_type != "STOP":
+            return
+
+        msg = JointTrajectory()
+        msg.header = Header()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.joint_names = self.joint_names
+        point = JointTrajectoryPoint()
+
         if position_type == "UP":
-            #Unies ramie (drugi staw na -1.57 rad)
+            # POZYCJA: PIONOWO (Na bacznosc)
+            # Drugi staw: -1.57 (Pionowo w gore)
             point.positions = [0.0, -1.57, 0.0, -1.57, 0.0, 0.0]
-            self.get_logger().info('Wysylam komende: JAZDA W GORE')
+            self.get_logger().info('>>> JAZDA W GORE')
             
         elif position_type == "DOWN":
-            #Opusc ramie (drugi staw na -0.5 rad)
+            # POZYCJA: POZIOMO (Opuszczony)
+            # Drugi staw: -0.5 (Pochylony do przodu/poziomu)
+            # Reszta bez zmian
             point.positions = [0.0, -0.5, 0.0, -1.57, 0.0, 0.0]
-            self.get_logger().info('Wysylam komende: JAZDA W DOL')
+            self.get_logger().info('>>> JAZDA W DOL')
             
-        else:
-            return 
-        #Czas na wykonanie ruchu
-        point.time_from_start.sec = 5
-        point.time_from_start.nanosec = 0
-        
+        elif position_type == "STOP":
+            self.stop_robot()
+            self.last_command = "STOP"
+            return
+
+        point.time_from_start.sec = 5 # Czas na dojazd
         msg.points = [point]
         self.publisher_.publish(msg)
         self.last_command = position_type
@@ -80,16 +112,13 @@ class ArucoNode(Node):
         current_frame = self.br.imgmsg_to_cv2(data, "bgr8")
         height, width, channels = current_frame.shape
         center_y = int(height / 2)
-        
         blue_color = (255, 0, 0)
-
+        
         top_threshold = center_y - self.dead_zone_pixels
         bottom_threshold = center_y + self.dead_zone_pixels
         
-        #Rysowanie linii
         cv2.line(current_frame, (0, top_threshold), (width, top_threshold), blue_color, 2)
         cv2.line(current_frame, (0, bottom_threshold), (width, bottom_threshold), blue_color, 2)
-        cv2.putText(current_frame, "STREFA STOP", (10, center_y + 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, blue_color, 1)
         
         arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
         arucoParams = cv2.aruco.DetectorParameters_create()
@@ -107,18 +136,19 @@ class ArucoNode(Node):
                 cv2.circle(current_frame, (cX, cY), 4, (0, 0, 255), -1)
 
                 if cY < top_threshold:
-                    cv2.putText(current_frame, "UP -> ROBOT GORA", (cX + 10, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+                    cv2.putText(current_frame, "UP", (cX + 10, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
                     self.send_robot_command("UP")
                     
                 elif cY > bottom_threshold:
-                    cv2.putText(current_frame, "DOWN -> ROBOT DOL", (cX + 10, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                    cv2.putText(current_frame, "DOWN", (cX + 10, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
                     self.send_robot_command("DOWN")
                     
                 else:
                     cv2.putText(current_frame, "STOP", (cX + 10, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.7, blue_color, 2)
-                    self.last_command = "STOP" 
+                    self.send_robot_command("STOP")
         else:
-            cv2.putText(current_frame, "BRAK ZNACZNIKA", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
+            # Pamieta ostatni ruch (brak komendy STOP tutaj)
+            cv2.putText(current_frame, "BRAK ZNACZNIKA - PAMIETAM RUCH", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
 
         cv2.imshow("Panel Sterowania UR5", current_frame)
         cv2.waitKey(1)
